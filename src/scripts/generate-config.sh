9c54e34f511b2d5392ee4141e7253b339272c55e
Jakub Matejka <215660+JakubMatejka@users.noreply.github.com>
2023-10-20 10:03:45 +0200

    separate build and push and define as reusable commands

--- src/scripts/generate-config.sh
+++ src/scripts/generate-config.sh
@@ -24,15 +24,15 @@ if [ "$test_unit" = "null" ]; then
     exit 1
 fi
 
-test_e2e=$(yq eval '.jobs.test_e2e' .circleci/app.yml)
-if [ "$test_e2e" = "null" ]; then
-    echo "Config .circleci/app.yml does not contain 'test_e2e' job"
-    exit 1
-fi
-
 build=$(yq eval '.jobs.build' .circleci/app.yml)
 if [ "$build" = "null" ]; then
     echo "Config .circleci/app.yml does not contain 'build' job"
+    exit 1
+fi
+
+push=$(yq eval '.jobs.push' .circleci/app.yml)
+if [ "$push" = "null" ]; then
+    echo "Config .circleci/app.yml does not contain 'push' job"
     exit 1
 fi
 
@@ -55,15 +55,35 @@ git checkout "$TF_WORKFLOW_GIT_REF"
 cd ..
 
 echo "version: 2.1" > /tmp/generated-config.yml
-
 tf=$(jq -r '.tf' "$CONFIG_JSON_FILE")
 if [ "$tf" == true ]; then
     echo "Terraform deployment triggered"
     # shellcheck disable=SC2016
-    yq eval-all --inplace '. as $item ireduce ({}; . * $item )' /tmp/generated-config.yml circle-ci-node-ecs-orb/workflows/app-tf.yml circle-ci-node-ecs-orb/workflows/jobs-tf.yml .circleci/app.yml
+    yq eval-all --inplace '. as $item ireduce ({}; . * $item )' /tmp/generated-config.yml circle-ci-node-ecs-orb/workflows/deploy-app-tf.yml circle-ci-node-ecs-orb/workflows/jobs-tf.yml .circleci/app.yml
 else
     # shellcheck disable=SC2016
-    yq eval-all --inplace '. as $item ireduce ({}; . * $item )' /tmp/generated-config.yml circle-ci-node-ecs-orb/workflows/app-only.yml .circleci/app.yml
+    yq eval-all --inplace '. as $item ireduce ({}; . * $item )' /tmp/generated-config.yml circle-ci-node-ecs-orb/workflows/deploy-app-only.yml .circleci/app.yml
+fi
+
+# Remove test_acceptance from workflow if not present in app.yml
+test_acceptance=$(yq eval '.jobs.test_acceptance' .circleci/app.yml)
+if [ "$test_acceptance" = "null" ]; then
+    yq --inplace 'del(.workflows.cicd_app.jobs[] | select(has("test_acceptance")))' /tmp/generated-config.yml
+    yq --inplace 'del(.workflows.cicd_app.jobs[] | select(has("push")) | .push.requires[] | select(. == "test_acceptance"))' /tmp/generated-config.yml
+fi
+
+# Remove test_e2e from workflow if not present in app.yml
+test_e2e=$(yq eval '.jobs.test_e2e' .circleci/app.yml)
+if [ "$test_e2e" = "null" ]; then
+    yq --inplace 'del(.workflows.cicd_app.jobs[] | select(has("test_e2e")))' /tmp/generated-config.yml
+    yq --inplace 'del(.workflows.cicd_app.jobs[] | select(has("push")) | .push.requires[] | select(. == "test_e2e"))' /tmp/generated-config.yml
+fi
+
+# Remove test_ui from workflow if not present in app.yml
+test_ui=$(yq eval '.jobs.test_ui' .circleci/app.yml)
+if [ "$test_ui" = "null" ]; then
+    yq --inplace 'del(.workflows.cicd_app.jobs[] | select(has("test_ui")))' /tmp/generated-config.yml
+    yq --inplace 'del(.workflows.cicd_app.jobs[] | select(has("push")) | .push.requires[] | select(. == "test_ui"))' /tmp/generated-config.yml
 fi
 
 lambda=$(jq -r '.lambda' "$CONFIG_JSON_FILE")
