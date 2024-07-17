96158c2e2051c654685dac24bf71a58f3f9ee359
Jakub Matějka <215660+JakubMatejka@users.noreply.github.com>
2024-07-17 18:07:07 +0200

    feat: allow export of Timescale credentials from SSM to TF (#60)

--- src/scripts/set-variables.sh
+++ src/scripts/set-variables.sh
@@ -5,3 +5,14 @@ echo "export DATADOG_API_KEY=$DATADOG_API_KEY" >> "$BASH_ENV"
 
 DATADOG_APP_KEY=$(aws ssm get-parameters --names "secret.datadog.terraform_app_key" --region="$AWS_REGION" --query 'Parameters[].Value' --output text --with-decryption --profile "$AWS_PROFILE")
 echo "export DATADOG_APP_KEY=$DATADOG_APP_KEY" >> "$BASH_ENV"
+
+if [ -n "$EXPORT_TIMESCALE_CREDENTIALS" ]; then
+    TF_VAR_timescale_project_id=$(aws ssm get-parameters --names "timescale_project_id" --region="$AWS_REGION" --query 'Parameters[].Value' --output text)
+    echo "export TF_VAR_timescale_project_id=$TF_VAR_timescale_project_id" >> "$BASH_ENV"
+
+    TF_VAR_timescale_public_key=$(aws ssm get-parameters --names "timescale_public_key" --region="$AWS_REGION" --query 'Parameters[].Value' --output text)
+    echo "export TF_VAR_timescale_public_key=$TF_VAR_timescale_public_key" >> "$BASH_ENV"
+
+    TF_VAR_timescale_secret_key=$(aws ssm get-parameters --names "timescale_secret_key" --region="$AWS_REGION" --query 'Parameters[].Value' --output text --with-decryption)
+    echo "export TF_VAR_timescale_secret_key=$TF_VAR_timescale_secret_key" >> "$BASH_ENV"
+fi
